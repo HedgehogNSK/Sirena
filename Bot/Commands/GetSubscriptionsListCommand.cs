8e2c8236cbd9b38633f3f3e19376cd9c03d57488
Vladislav Ivashnev <hedgensk@gmail.com>
2024-05-17 17:19:17 +0700

    Update GetSubscriptionsListCommand

--- Bot/Commands/GetSubscriptionsListCommand.cs
+++ Bot/Commands/GetSubscriptionsListCommand.cs
@@ -1,67 +1,51 @@
+using Hedgey.Sirena.Bot.Operations;
 using Hedgey.Sirena.Database;
-using MongoDB.Driver;
 using RxTelegram.Bot.Interface.BaseTypes;
-using System.Text;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace Hedgey.Sirena.Bot;
-public class GetSubscriptionsListCommand : AbstractBotCommmand, IBotCommand
+public class GetSubscriptionsListCommand : AbstractBotCommmand, IBotCommand, IDisposable
 {
   public const string NAME = "subscriptions";
   public const string DESCRIPTION = "Displays you current subscriptions.";
+  CompositeDisposable disposables = new CompositeDisposable();
+  private readonly IMessageSender messageSender;
+  private readonly IGetUserRelatedSirenas findSirena;
+  private readonly IGetUserInformation getUserInformation;
 
-  private IMongoCollection<UserRepresentation> usersCollection;
-  private IMongoCollection<SirenRepresentation> sirenCollection;
-
-  public GetSubscriptionsListCommand(IMongoDatabase db)
-  : base(NAME, DESCRIPTION)
+  public GetSubscriptionsListCommand(IMessageSender messageSender
+  , IGetUserRelatedSirenas findSirena, IGetUserInformation getUserInformation)
+    : base(NAME, DESCRIPTION)
   {
-    usersCollection = db.GetCollection<UserRepresentation>("users");
-    sirenCollection = db.GetCollection<SirenRepresentation>("sirens");
+    this.messageSender = messageSender;
+    this.findSirena = findSirena;
+    this.getUserInformation = getUserInformation;
   }
 
-  public async override void Execute(IRequestContext context)
+  public override void Execute(IRequestContext context)
   {
     User botUser = context.GetUser();
     long uid = botUser.Id;
     long chatId = context.GetChat().Id;
-    var filterBuilder = Builders<SirenRepresentation>.Filter;
-    var currentUserFilter = filterBuilder.Exists(x => x.Listener)
-        & filterBuilder.AnyEq(x => x.Listener, uid);
-    var subscriptionList = await sirenCollection.Find(currentUserFilter).ToListAsync();
-    if (!subscriptionList.Any())
-    {
-      string emptyListMessage = "You don't have any subscriptions";
-      Program.messageSender.Send(chatId, emptyListMessage);
-      return;
-    }
 
-    string messageText = CreateMessageText(subscriptionList);
-    Program.messageSender.Send(chatId, messageText);
+    IDisposable userSubscriptionsStream = findSirena.GetSubscriptions(uid)
+      .SelectMany(_sirenas => _sirenas)
+      .SelectMany(_sirena => getUserInformation.GetNickname(_sirena.OwnerId)
+          .Select(_nick => (_sirena, _nick)))
+      .ToArray().Subscribe(_subscriptions => ProcessResult(_subscriptions, chatId));
+
+    disposables.Add(userSubscriptionsStream);
   }
 
-  private static string CreateMessageText(List<SirenRepresentation> userSirens)
+  private void ProcessResult((SirenRepresentation _sirena, string _nick)[] subscriptions, long chatId)
   {
-    StringBuilder builder = new StringBuilder();
-    if (userSirens.Count != 0)
-    {
-      int number = 0;
-      builder.Append("Your subscriptions:\n");
-      foreach (var siren in userSirens)
-      {
-        ++number;
-        builder.Append(number).Append(' ').Append(siren.Id)
-        .Append(' ')
-        .Append('*')
-        .Append(siren.Title)
-        .Append('*')
-        .Append('\n');
-      }
-    }
-    else
-    {
-      builder.Append("You didn't subscribed on any sirenas yet.");
-    }
-    var messageText = builder.ToString();
-    return messageText;
+    MessageBuilder builder = new SubscriptionsMesssageBuilder(chatId, subscriptions);
+    messageSender.Send(builder.Build());
+  }
+
+  public void Dispose()
+  {
+    disposables?.Dispose();
   }
 }
--- Bot/Messages/UnsubscribeFromSirena/SubscriptionsMesssageBuilder.cs
+++ Bot/Messages/UnsubscribeFromSirena/SubscriptionsMesssageBuilder.cs
@@ -6,7 +6,6 @@ using System.Reactive.Linq;
 using System.Text;
 
 namespace Hedgey.Sirena.Bot;
-
 internal class SubscriptionsMesssageBuilder : MessageBuilder
 {
   private IEnumerable<(SirenRepresentation sirena, string nickname)> source;
@@ -21,12 +20,11 @@ internal class SubscriptionsMesssageBuilder : MessageBuilder
   public override SendMessage Build()
   {
     StringBuilder builder = new StringBuilder();
-    if (source.Any())
+    if (source.Skip(1).Any())
     {
       const int buttonsPerLine = 5;
-      const string template = ". `{0}` {1}\n*{2}*\n\n";
-      const string prefix = "Your subscriptions:\n";
-      const string notification = "You can press button to 🔕 *UNSUBSCRIBE* from a certain Sirena.";
+      const string template = ". *{0}* by {1}\nID: `{2}`\n\n";
+      const string prefix = "*Your subscriptions:*\n\n";
 
       int number = 0;
       builder.Append(prefix);
@@ -38,15 +36,17 @@ internal class SubscriptionsMesssageBuilder : MessageBuilder
         {
           keyboardBuilder.EndRow().BeginRow();
         }
+        keyboardBuilder.AddSirenaInfoButton(tuple.sirena.Id,number.ToString());
+        
         builder.Append(number)
-          .AppendFormat(template, tuple.sirena.Id, tuple.nickname, tuple.sirena.Title);
+          .AppendFormat(template, tuple.sirena.Title, tuple.nickname, tuple.sirena.Id);
 
-        keyboardBuilder.AddCallbackData(number.ToString()
-        , '/' + UnsubscribeCommand.NAME + ' ' + tuple.sirena.Id.ToString());
       }
-
-      builder.Append(notification);
       return CreateDefault(builder.ToString(), keyboardBuilder.EndRow().ToReplyMarkup());
+    }
+    else if(source.Any()){
+      var subscription =source.First();
+      return new  SirenaInfoMessageBuilder(chatId, chatId, subscription.sirena).Build();
     }
     else
     {
