f5f25677b389691a0a13405f447e2d9f507d88a7
Vladislav Ivashnev <hedgensk@gmail.com>
2024-05-26 00:14:44 +0700

    Modify SirenaOperation methods Task convertation method .ToObservable replace with Observable.FromAsync

--- Bot/Operations/Mongo/SirenaOperations.cs
+++ Bot/Operations/Mongo/SirenaOperations.cs
@@ -2,7 +2,6 @@ using Hedgey.Sirena.Database;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace Hedgey.Sirena.Bot.Operations.Mongo;
@@ -31,32 +30,32 @@ public class SirenaOperations : IDeleteSirenaOperation
   public IObservable<SirenRepresentation> DeleteSirenaDocument(ObjectId id)
   {
     var sirenFilter = Builders<SirenRepresentation>.Filter.Eq(x => x.Id, id);
-    return sirens.FindOneAndDeleteAsync(sirenFilter).ToObservable();
+    return Observable.FromAsync(() =>sirens.FindOneAndDeleteAsync(sirenFilter));
   }
 
   public IObservable<UpdateResult> DeleteSirenaIdFromOwner(long uid, ObjectId id)
   {
     var filter = Builders<UserRepresentation>.Filter.Eq(x => x.UID, uid);
     var userUpdate = Builders<UserRepresentation>.Update.Pull(x => x.Owner, id);
-    return users.UpdateOneAsync(filter, userUpdate).ToObservable();
+    return Observable.FromAsync(() =>users.UpdateOneAsync(filter, userUpdate));
   }
   public IObservable<SirenRepresentation> Subscribe(long uid, ObjectId id)
   {
     var filterSiren = Builders<SirenRepresentation>.Filter.Eq(x => x.Id, id) 
         & Builders<SirenRepresentation>.Filter.Ne(x=>x.OwnerId, uid);
     var addSubsription = Builders<SirenRepresentation>.Update.AddToSet(x => x.Listener, uid);
-    return sirens.FindOneAndUpdateAsync(filterSiren, addSubsription).ToObservable();
+    return Observable.FromAsync(() =>sirens.FindOneAndUpdateAsync(filterSiren, addSubsription));
   }
   public IObservable<bool> Unsubscribe(long uid, ObjectId id)
   {
     var filter = Builders<SirenRepresentation>.Filter.Eq(x => x.Id, id);
     var update = Builders<SirenRepresentation>.Update.Pull(x => x.Listener, uid);
-    return sirens.UpdateOneAsync(filter, update).ToObservable().Select(x => x.ModifiedCount != 0);
+    return Observable.FromAsync(() =>sirens.UpdateOneAsync(filter, update)).Select(x => x.ModifiedCount != 0);
   }
   public IObservable<SirenRepresentation> Find(ObjectId id)
   {
     var filterSiren = Builders<SirenRepresentation>.Filter.Eq(x => x.Id, id);
-    return sirens.Find(filterSiren).FirstOrDefaultAsync().ToObservable();
+    return Observable.FromAsync(() =>sirens.Find(filterSiren).FirstOrDefaultAsync());
   }
 
   public IObservable<List<SirenRepresentation>> Find(string keyPhrase)
@@ -65,17 +64,17 @@ public class SirenaOperations : IDeleteSirenaOperation
     var pattern = new Regex(formatedKey, RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Multiline);
     var bsonRegex = new BsonRegularExpression(pattern);
     var filter = Builders<SirenRepresentation>.Filter.Regex(x => x.Title, bsonRegex);
-    return sirens.Find(filter).ToListAsync().ToObservable();
+    return Observable.FromAsync(() =>sirens.Find(filter).ToListAsync());
   }
   public IObservable<IEnumerable<SirenRepresentation>> GetUserSirenas(long uid)
   {
     var filter = Builders<SirenRepresentation>.Filter.Eq(x => x.OwnerId, uid);
-    return sirens.Find(filter).ToListAsync().ToObservable();
+    return Observable.FromAsync(() =>sirens.Find(filter).ToListAsync());
   }
   public IObservable<IEnumerable<SirenRepresentation>> GetSubscriptions(long uid)
   {
     var filter = Builders<SirenRepresentation>.Filter.AnyEq(x => x.Listener, uid);
-    return sirens.Find(filter).ToListAsync().ToObservable();
+    return Observable.FromAsync(() =>sirens.Find(filter).ToListAsync());
   }
   public IObservable<SirenRepresentation> GetUserSirena(long uid, int number)
   {
@@ -84,13 +83,13 @@ public class SirenaOperations : IDeleteSirenaOperation
       return Observable.Throw<SirenRepresentation>(new ArgumentException("Serial number has to be positive", nameof(number)));
 
     var filterSiren = Builders<SirenRepresentation>.Filter.Eq(x => x.OwnerId, uid);
-    return sirens.Find(filterSiren).Skip(number).FirstOrDefaultAsync().ToObservable();
+    return Observable.FromAsync(() =>sirens.Find(filterSiren).Skip(number).FirstOrDefaultAsync());
   }
 
   public IObservable<SirenRepresentation> UpdateLastCall(ObjectId sirenId, SirenRepresentation.CallInfo  callInfo)
   {
     var filter = Builders<SirenRepresentation>.Filter.Eq(x => x.Id, sirenId);
     var update = Builders<SirenRepresentation>.Update.Set(x => x.LastCall, callInfo);
-    return sirens.FindOneAndUpdateAsync(filter, update).ToObservable();
+    return Observable.FromAsync(() =>sirens.FindOneAndUpdateAsync(filter, update));
   }
 }
