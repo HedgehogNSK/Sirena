a43d508bd90c635351737f56793cb64443f2537a
Vladislav Ivashnev <hedgensk@gmail.com>
2024-07-05 17:14:32 +0700

    Update CreateSirenaCommand base class -> PlanExecutorBotCommand

--- Bot/Commands/CreateSirena/CreateSirenCommand.cs
+++ Bot/Commands/CreateSirena/CreateSirenCommand.cs
@@ -2,23 +2,13 @@ using Hedgey.Structure.Factory;
 
 namespace Hedgey.Sirena.Bot;
 
-public class CreateSirenaCommand : AbstractBotCommmand
+public class CreateSirenaCommand : PlanExecutorBotCommand
 {
   public const string NAME = "create";
   public const string DESCRIPTION = "Creates a sirena with certain title";
-  private readonly IFactory<IRequestContext, CommandPlan> planFactory;
-  private readonly PlanScheduler planScheduler;
 
-  public CreateSirenaCommand(IFactory<IRequestContext, CommandPlan> planFactory,
-PlanScheduler planScheduler)
-  : base(NAME, DESCRIPTION)
-  {
-    this.planFactory = planFactory;
-    this.planScheduler = planScheduler;
-  }
-  public override void Execute(IRequestContext context)
-  {
-    var plan = planFactory.Create(context);
-    planScheduler.Push(plan);
-  }
+  public CreateSirenaCommand(IFactory<IRequestContext, CommandPlan> planFactory
+    , PlanScheduler planScheduler)
+   : base(NAME, DESCRIPTION, planFactory, planScheduler)
+  { }
 }
