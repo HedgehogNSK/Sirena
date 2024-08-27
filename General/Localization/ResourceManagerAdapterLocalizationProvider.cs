c37f6e188e084f9b9256620c0b5ba0ec492cfb22
Vladislav Ivashnev <hedgensk@gmail.com>
2024-08-28 00:38:48 +0700

    Add logging for missed localization

--- General/Localization/ResourceManagerAdapterLocalizationProvider.cs
+++ General/Localization/ResourceManagerAdapterLocalizationProvider.cs
@@ -3,16 +3,27 @@ using System.Resources;
 
 namespace Hedgey.Localization;
 
-public class ResourceManagerAdapterLocalizationProvider : ILocalizationProvider{
+public class ResourceManagerAdapterLocalizationProvider : ILocalizationProvider
+{
   public readonly ResourceManager rm;
-  public CultureInfo? CurrentCulture { get; private set; } =null;
-  public ResourceManagerAdapterLocalizationProvider(ResourceManager rm) {
+  public CultureInfo? CurrentCulture { get; private set; } = null;
+  public ResourceManagerAdapterLocalizationProvider(ResourceManager rm)
+  {
     this.rm = rm;
   }
 
-  public string Get(string key, CultureInfo info) => rm.GetString(key, info) ?? key;
+  public string Get(string key, CultureInfo info)
+  {
+    string? result = rm.GetString(key, info);
+    if (result == null)
+    {
+      result = key;
+      Console.WriteLine($"Localization key: {key} has no pair");
+    }
+    return result;
+  }
 
-  public string GetCurrent(string key) => rm.GetString(key,CurrentCulture) ?? key;
+  public string GetCurrent(string key) => rm.GetString(key, CurrentCulture) ?? key;
 
   public void SetCulture(CultureInfo info)
   {
