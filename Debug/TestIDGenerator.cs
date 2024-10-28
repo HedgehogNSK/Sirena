e05dadd4f29f9bbcbc914a7194c4fdbcb4c9176a
Vladislav Ivashnev <hedgensk@gmail.com>
2024-10-29 00:38:54 +0700

    Update test methods

--- Debug/TestIDGenerator.cs
+++ Debug/TestIDGenerator.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Hedgey.Extensions;
 using Hedgey.Tools.BlendedFlake;
 using Hedgey.Utilities;
@@ -6,6 +7,9 @@ namespace Hedgey.Debug;
 
 static public class TestIDGenerator
 {
+  private static Generator generator;
+
+  static public void SetGenerator(Generator generator) { TestIDGenerator.generator = generator; }
   static public void Display(ulong id)
   {
     string result = Base64UrlMod.FromLong(id); // Get Base64 URL Hedgey modification string
@@ -13,8 +17,18 @@ static public class TestIDGenerator
     string shortResult = HashUtilities.Shortify(result);
     string restoreResult = HashUtilities.Expand(shortResult);
     ulong number = (ulong)Base64UrlMod.ToLong(restoreResult);
-    Console.WriteLine($"Source ID:{id}\nBits [Low Ending]: {Bit.ToString(id, false)}\nBase64URLHM hash: {result} | Short variant:\t{shortResult}\nResotred hash:\t{restoreResult} | Restored id:\t{number}\nRestored bits: {Bit.ToString(number, false)}\n﹋﹋﹋﹋﹋");
+    var machineID = Generator.ComputeMachineID(id);
+    var sequence = Generator.ComputeSequenceNumber(id);
+    var timestamp = Generator.ComputeCreationTimestamp(id, generator.epoch);
+    DateTimeOffset date = DateTimeOffset.FromUnixTimeMilliseconds((long)timestamp);
+    Console.WriteLine($"Source ID:{id}\nBits [Low Ending]: {Bit.ToString(id, false)}\nBase64URLHM hash: {result} | Short variant:\t{shortResult}\nResotred hash:\t{restoreResult} | Restored id:\t{number}\nRestored bits: {Bit.ToString(number, false)}\nMachine ID: {machineID} | {sequence} | Date: {date}[{timestamp}]\n﹋﹋﹋﹋﹋");
     if (number != id)
       throw new InvalidOperationException($"ID conversion failed. Source ID:  {id}; Restored: {number}");
+  }
+  static public void TestBase64(string test)
+  {
+    var bytes = Encoding.ASCII.GetBytes(test);
+    var hash = Base64UrlMod.FromBytes(bytes);
+    Console.WriteLine(hash);
   }
 }
