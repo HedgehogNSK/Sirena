05122541c56ebf93153d1577823be4e60a8ff724
Vladislav Ivashnev <hedgensk@gmail.com>
2025-01-15 03:22:13 +0700

    Update /call with hash id

--- Bot/Commands/CallSirena/Messages/StringNotIdMessageBuilder.cs
+++ Bot/Commands/CallSirena/Messages/StringNotIdMessageBuilder.cs
@@ -1,37 +1,94 @@
 using Hedgey.Localization;
+using Hedgey.Sirena.Database;
 using Hedgey.Structure.Factory;
+using RxTelegram.Bot.Interface.BaseTypes.Requests.Base.Interfaces;
 using RxTelegram.Bot.Interface.BaseTypes.Requests.Messages;
 using RxTelegram.Bot.Utils.Keyboard;
 using System.Globalization;
+using System.Text;
 
 namespace Hedgey.Sirena.Bot;
 
 public class StringNotIdMessageBuilder : LocalizedMessageBuilder
 {
-  private string param;
+  private IEnumerable<SirenRepresentation> sirens;
 
   public StringNotIdMessageBuilder(long chatId, CultureInfo info
-  , ILocalizationProvider localizationProvider, string param)
+  , ILocalizationProvider localizationProvider
+  , IEnumerable<SirenRepresentation> sirens)
   : base(chatId, info, localizationProvider)
   {
     this.chatId = chatId;
-    this.param = param;
+    this.sirens = sirens;
   }
 
   public override SendMessage Build()
   {
-    string paramIncorrect = Localize("command.call.error.value_not_id");
-    string notification = Localize("command.call.ask_sirena_id");
-    string message = string.IsNullOrEmpty(param) ? notification : paramIncorrect + notification;
+    StringBuilder builder = new StringBuilder();
 
-    var markup = KeyboardBuilder.CreateInlineKeyboard().BeginRow()
-      .AddDisplayUserSirenasButton(Info).AddMenuButton(Info).EndRow().ToReplyMarkup();
-    return CreateDefault(message, markup);
+    var keyboardBuilder = KeyboardBuilder.CreateInlineKeyboard().BeginRow();
+    if (sirens.Any())
+    {
+      const string template = ". `{0}` *{1}*\n";
+      string subscribers = Localize("command.display_sirenas.subscribers");
+      string listIntroduction = Localize("command.call.available.description");
+
+      //Evaluate buttons per line
+      const float maxPerLine = 5f;
+      int total = sirens.Count();
+      int lines = (int)MathF.Ceiling(total / maxPerLine);
+      if (total > 2 && lines == 1) lines = 2;
+      int extra = total % lines;
+      int buttonsPerLine = total / lines + 1;
+      if (extra > 0)
+        ++buttonsPerLine;
+
+      int number = 0;
+      int prevNumber = 0;
+      builder.AppendLine(listIntroduction).AppendLine();
+      foreach (var sirena in sirens)
+      {
+        ++number;
+        //If the row is full
+        if ((number - prevNumber) % buttonsPerLine == 0)
+        {
+          //If long rows not null
+          if (extra > 0)
+          {
+            --extra;
+            //When extras ends up decrease buttonsPerLine
+            if (extra == 0)
+            {
+              --buttonsPerLine;
+              prevNumber = number;
+            }
+          }
+          keyboardBuilder.EndRow().BeginRow();
+        }
+        keyboardBuilder.AddButton(number, CallSirenaCommand.NAME, sirena.ShortHash);
+
+        builder.Append(number).AppendFormat(template, sirena.ShortHash, sirena.Title);
+        if (sirena.Listener.Length != 0)
+          builder.AppendFormat(subscribers, sirena.Listener.Length);
+        builder.AppendLine();
+      }
+      IReplyMarkup replyMarkup = keyboardBuilder.EndRow().ToReplyMarkup();
+
+      var messageText = builder.ToString();
+      return CreateDefault(messageText, replyMarkup);
+    }
+    else
+    {
+      string noAvailable = Localize("command.call.no_available");
+      var markup = KeyboardBuilder.CreateInlineKeyboard().BeginRow()
+          .AddCreateButton(Info).AddMenuButton(Info).EndRow().ToReplyMarkup();
+      return CreateDefault(noAvailable, markup);
+    }
   }
   public class Factory(ILocalizationProvider localizationProvider)
-    : IFactory<IRequestContext, string, StringNotIdMessageBuilder>
+    : IFactory<IRequestContext, IEnumerable<SirenRepresentation>, StringNotIdMessageBuilder>
   {
-    public StringNotIdMessageBuilder Create(IRequestContext context, string param)
+    public StringNotIdMessageBuilder Create(IRequestContext context, IEnumerable<SirenRepresentation> param)
     {
       var chatId = context.GetChat().Id;
       var info = context.GetCultureInfo();
--- Bot/Commands/FindSirena/Messages/NoSirenaMessageBuilder.cs
+++ Bot/Commands/FindSirena/Messages/NoSirenaMessageBuilder.cs
@@ -6,24 +6,17 @@ namespace Hedgey.Sirena.Bot;
 
 public class NoSirenaMessageBuilder : LocalizedMessageBuilder
 {
-  private readonly ulong id;
   string key = string.Empty;
   public NoSirenaMessageBuilder(long chatId, CultureInfo info
   , ILocalizationProvider localizationProvider, string key) : base(chatId, info, localizationProvider)
   {
     this.key = key;
   }
-  public NoSirenaMessageBuilder(long chatId, CultureInfo info
-  , ILocalizationProvider localizationProvider, ulong id) : base(chatId, info, localizationProvider)
-  {
-    this.id = id;
-  }
-
   public override SendMessage Build()
   {
     string noSirenaByKeyError = Localize("command.find.no_title");
     string noSirenaByIdError = Localize("command.find.no_id");
-    string message = id != default ? string.Format(noSirenaByIdError, id)
+    string message = !string.IsNullOrEmpty(key) ? string.Format(noSirenaByIdError, key)
       : string.Format(noSirenaByKeyError, key);
     return CreateDefault(message, MarkupShortcuts.CreateMenuButtonOnlyMarkup(Info));
   }
