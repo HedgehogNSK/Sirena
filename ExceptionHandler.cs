c479df505b7b9cfaa37ee796cdef94e1000ba889
Vladislav Ivashnev <hedgensk@gmail.com>
2025-02-27 21:35:19 +0200

    Minor formating changes ExceptionHandler

--- ExceptionHandler.cs
+++ ExceptionHandler.cs
@@ -5,22 +5,24 @@ namespace Hedgey.Sirena;
 
 static public class ExceptionHandler
 {
-  static public void OnError(Exception exception){
+  static public void OnError(Exception exception)
+  {
     var time = Shortucts.CurrentTimeLabel();
     var current = exception;
     Console.WriteLine(time + "EXCEPTION!!!");
-    int i=0;
+    int i = 0;
     do
     {
+      string? name = current.GetType().FullName;
       switch (current)
       {
         case ApiException apiException:
           {
-            string message = $"<{i}> {current.GetType().FullName}: {apiException.Message}\nDescription: {apiException.Description}\n{apiException.StackTrace}";
+            string message = $"<{i}> {name}: {current.Message}\nDescription: {apiException.Description}\n{current.StackTrace}";
             Console.WriteLine(message);
           }
           break;
-        default: Console.WriteLine($"<{i}> {current.GetType().FullName}: {current.Message}\n{current.StackTrace}"); break;
+        default: Console.WriteLine($"<{i}> {name}: {current.Message}\n{current.StackTrace}"); break;
       }
       current = current.InnerException;
       ++i;
